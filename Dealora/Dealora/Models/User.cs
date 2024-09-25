9292eab6f585bdd01d76aaebcc77bbe2ff08e273
Safkat Mahmud Sakib <safkatmahmudsakib@gamil.com>
2024-09-26 05:10:43 +0600

    Change Password

--- Dealora/Dealora/Models/User.cs
+++ Dealora/Dealora/Models/User.cs
@@ -15,38 +15,19 @@ namespace Dealora.Models
     {
         public int Id { get; set; }
 
-        [Display(Name ="First Name")]
-        [Required(ErrorMessage = "First Name is required")]
-        [StringLength(50, ErrorMessage = "First Name cannot be longer than 50 characters")]
+
         public string FirstName { get; set; }
 
-        [Display(Name = "Last Name")]
-        [Required(ErrorMessage = "Last Name is required")]
-        [StringLength(50, ErrorMessage = "Last Name cannot be longer than 50 characters")]
+
         public string LastName { get; set; }
 
-        [Display(Name = "Email")]
-        [Required(ErrorMessage = "Email is required")]
-        [EmailAddress(ErrorMessage = "Invalid Email Address")]
-        [StringLength(100, ErrorMessage = "Email cannot be longer than 100 characters")]
+
         public string Email { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        [Required(ErrorMessage = "Password is required")]
-        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 100 characters")]
+
         public string Password { get; set; }
 
-        [NotMapped]
-        [Display(Name = "Confirm Password")]
-        [DataType(DataType.Password)]
-        [Required(ErrorMessage = "Confirm Password is required")]
-        [Compare("Password", ErrorMessage = "Password and Confirm Password do not match")]
-        public string ConfirmPassword { get; set; }
 
-        [Display(Name = "Mobile No.(Optional)")]
-        [Phone(ErrorMessage = "Invalid Phone Number")]
-        [StringLength(20, ErrorMessage = "Phone Number cannot be longer than 20 characters")]
         public string PhoneNumber { get; set; }
 
         public UserRole Role { get; set; }
