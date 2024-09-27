7bf980717b784317364df83328a0c5ace2d2308b
Safkat Mahmud Sakib <safkatmahmudsakib@gamil.com>
2024-09-28 02:25:47 +0600

    Address Done

---
+++ Dealora/Dealora/Controllers/AddresseController.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Dealora.Context;
+using Dealora.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Dealora.ViewModels;
+using Dealora.Models.ViewModel;
+
+namespace Dealora.Controllers
+{
+    public class AddresseController : Controller
+    {
+        private DealoraAppDbContext db = new DealoraAppDbContext();
+        private HttpClient client;
+
+        public AddresseController()
+        {
+            this.client = new HttpClient();
+            this.client.BaseAddress = new Uri(@"http://localhost:9570/api/");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            if (Session["JWTToken"] != null)
+            {
+                // Set Authorization header with JWT token
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Session["JWTToken"].ToString());
+
+                // Assuming userId is stored in the session
+                int userId = (int)Session["UserId"];
+
+                // Call the API to get the single address by userId
+                var response = await client.GetAsync($"Addresses/{userId}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // Deserialize the response to a single Address object
+                    var address = await response.Content.ReadAsAsync<Address>();
+                    return View(address);  // Pass the single address to the view
+                }
+                else if (response.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    return RedirectToAction("Unauthorized", "Home");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            else
+            {
+                return RedirectToAction("Login", "User");
+            }
+        }
+
+
+
+
+
+        // GET: Addresse/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            if (Session["JWTToken"] != null)
+            {
+                var cities = new List<string>
+        {
+            "Dhaka",
+            "Chittagong",
+            "Khulna",
+            "Rajshahi",
+            "Sylhet",
+            "Barisal",
+            "Rangpur",
+            "Comilla",
+            "Mymensingh"
+        };
+
+                ViewBag.Cities = new SelectList(cities);
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login","User");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Address address)
+        {
+            if (Session["UserId"] != null)
+            {
+                int userId = (int)Session["UserId"];
+                address.UserId = userId;  // Assign UserId to the address model
+
+                if (ModelState.IsValid)
+                {
+                    var response = client.PostAsJsonAsync("addresses", address);
+                    response.Wait();
+
+                    if (response.Result.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        return HttpNotFound();
+                    }
+                }
+
+                return View(address);  // In case ModelState is invalid
+            }
+            else
+            {
+                return RedirectToAction("Login", "User");  // Redirect to login if session expired
+            }
+        }
+
+
+        private List<string> GetCities()
+        {
+            return new List<string>
+        {
+            "Dhaka",
+            "Chittagong",
+            "Khulna",
+            "Rajshahi",
+            "Sylhet",
+            "Barisal",
+            "Rangpur",
+            "Comilla",
+            "Mymensingh"
+        };
+        }
+
+        // GET: Addresse/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (Session["JWTToken"] != null)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+
+                Address address = await db.Addresses.FindAsync(id);
+                if (address == null)
+                {
+                    return HttpNotFound();
+                }
+
+                // Map the Address entity to AddressUpdateModel
+                var addressViewModel = new AddressUpdateModel
+                {
+                    Id = address.Id,
+                    StreetAddress = address.StreetAddress,
+                    City = address.City
+                };
+
+                ViewBag.Cities = new SelectList(GetCities(), address.City);  // Pre-select the existing city
+                return View(addressViewModel);
+            }
+            else
+            {
+                return RedirectToAction("Login", "User");
+            }
+        }
+
+
+        // POST: Addresse/Edit/5
+        // POST: Addresse/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(AddressUpdateModel addressViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var address = await db.Addresses.FindAsync(addressViewModel.Id);
+                if (address == null)
+                {
+                    return HttpNotFound();
+                }
+
+                // Update address fields except for UserId
+                address.StreetAddress = addressViewModel.StreetAddress;
+                address.City = addressViewModel.City;
+
+                db.Entry(address).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Cities = new SelectList(GetCities(), addressViewModel.City);  // Repopulate cities in case of an error
+            return View(addressViewModel);
+        }
+
+
+
+        // GET: Addresse/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Address address = await db.Addresses.FindAsync(id);
+            if (address == null)
+            {
+                return HttpNotFound();
+            }
+            return View(address);
+        }
+
+        // POST: Addresse/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Address address = await db.Addresses.FindAsync(id);
+            db.Addresses.Remove(address);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Dealora/Dealora/Controllers/Api/AddressesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Dealora.Context;
+using Dealora.Models;
+
+namespace Dealora.Controllers.API
+{
+    public class AddressesController : ApiController
+    {
+        private DealoraAppDbContext db = new DealoraAppDbContext();
+
+        // GET: api/Addresses/User/{userId}
+        [HttpGet]
+        [Route("api/Addresses/{userId}")]
+        public IHttpActionResult GetAddressesByUserId(int userId)
+        {
+            var address = db.Addresses.FirstOrDefault(a => a.UserId == userId);  // Fetching a single address
+
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(address);
+        }
+
+
+
+
+        // PUT: api/Addresses/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutAddress(int id, Address address)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != address.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(address).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AddressExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Addresses
+        [ResponseType(typeof(Address))]
+        public async Task<IHttpActionResult> PostAddress(Address address)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Addresses.Add(address);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = address.Id }, address);
+        }
+
+        // DELETE: api/Addresses/5
+        [ResponseType(typeof(Address))]
+        public async Task<IHttpActionResult> DeleteAddress(int id)
+        {
+            Address address = await db.Addresses.FindAsync(id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            db.Addresses.Remove(address);
+            await db.SaveChangesAsync();
+
+            return Ok(address);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AddressExists(int id)
+        {
+            return db.Addresses.Count(e => e.Id == id) > 0;
+        }
+    }
+}
