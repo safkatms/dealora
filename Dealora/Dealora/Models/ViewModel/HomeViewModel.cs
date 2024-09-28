6f244daabb4af63b6338c1d26f55c31e1fbe28d6
Safkat Mahmud Sakib <safkatmahmudsakib@gamil.com>
2024-09-28 09:34:56 +0600

    Landing Page Updated

---
+++ Dealora/Dealora/Models/ViewModel/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dealora.Models.ViewModel
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
